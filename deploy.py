a01be5aedb1871d24d35e6a674f841f850934141
wunmis1 <94202477+wunmis1@users.noreply.github.com>
2021-11-21 15:34:47 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,2 +1,4 @@
 this file is the deplyment script for team 15 projects 
 ensure that the file is updated every time ther is new script addition.
+also ensure that the file is uplodaded to the remote repo
+before build is done
